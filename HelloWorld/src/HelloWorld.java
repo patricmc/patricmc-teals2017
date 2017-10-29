69c25bc3d1e9c9daaae2e757be37ada2eebf23b5
Patric McElroy <patric_mcelroy@hotmail.com>
2017-10-28 17:23:27 -0700

    Got rid of the second function

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,13 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		System.out.println("...now Git!");
-		main2();
+		//main2();
 		return
 	}
 
-	public static void main2(String[] args) {
-		System.out.println("Hello, bubba");
-		return;
-	}
-	
 }
