4a39cb28e1b70bf8b71c0482e98c3178ea1fda1d
Patric McElroy <patric_mcelroy@hotmail.com>
2017-10-28 17:18:42 -0700

    Added a second function - just for fun

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		System.out.println("...now Git!");
+		main2();
+		return
+	}
+
+	public static void main2(String[] args) {
+		System.out.println("Hello, bubba");
+		return;
 	}
 	
 }
